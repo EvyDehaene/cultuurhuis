4f00abd56b36ff70a7e6590c56d32f4964213e9c
evy.dehaene <evy.dehaene@PAUL-UUASVQ1FLL.vdabheverlee.intra>
2014-04-17 10:12:11 +0200

    17/04/2014 2

---
+++ cultuurhuis/src/be/vdab/entities/Genre.java
@@ -0,0 +1,20 @@
+package be.vdab.entities;
+
+public class Genre {
+	private int genreNr;
+	private String naam;
+	
+	public int getGenreNr() {
+		return genreNr;
+	}
+	public void setGenreNr(int genreNr) {
+		this.genreNr = genreNr;
+	}
+	public String getNaam() {
+		return naam;
+	}
+	public void setNaam(String naam) {
+		this.naam = naam;
+	}
+
+}
